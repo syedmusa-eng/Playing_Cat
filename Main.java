3b4ba6400d0c23f04ca9a69e9be1da077f7e75a2
SYED MUSA RAZA <79834398+syedmusa-eng@users.noreply.github.com>
2021-05-07 08:14:56 +0200

    TestApp
    Test app  to test the program

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+
+        //Calling the method to check the results
+        System.out.println(PlayingCat.isCatPlaying(true, 10));
+        System.out.println(PlayingCat.isCatPlaying(false, 36));
+        System.out.println(PlayingCat.isCatPlaying(false, 35));
+
+    }
+}
